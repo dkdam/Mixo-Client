3a96f3685bd9634ddf105b4055b8849c3e17c430
dkdam <dkdam@live.com>
2022-11-13 23:34:37 +1100

    successfully called api

--- src/components/DisplayMixologists.js
+++ src/components/DisplayMixologists.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 
 const DisplayMixologists = ()  => {
     const [mixologists, setMixologists] = useState([])
@@ -15,7 +15,26 @@ const DisplayMixologists = ()  => {
     }, []);
 
     return (
-        <h1>coming soon</h1>
+        <div>
+            {
+                mixologists.map((item) => {
+                    let name = <React.Fragment key ={item.id}>
+                    <h1>{item.name}</h1>    
+                    </React.Fragment>
+                    let drinksList = item.drinks.map(drink => {
+                        let drinksInfo = <React.Fragment key={drink.id}>
+                            <h3>{drink.name}</h3>
+                            <p>{drink.ingredients}</p>
+                        </React.Fragment>
+                        return drinksInfo
+                    })
+                    if (drinksList[0]) {
+                        return [name, drinksList]
+                    }
+                })
+                
+            }
+        </div>
     )
 }
 
